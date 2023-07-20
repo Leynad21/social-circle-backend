6370d63d1ceb56abb625f5726031a34716511ce3
Dany <dany21sayan_@hotmail.com>
2023-07-20 15:54:38 +0200

    settings update

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 # ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
